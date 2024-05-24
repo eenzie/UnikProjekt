fb3c888938f8bd8fa7d52afea40b9e91b51c8b55
eenzie <100802909+eenzie@users.noreply.github.com>
2024-05-24 10:45:21 +0200

    Document migration added

--- UnikProjekt.DatabaseMigration/Migrations/20240524082241_documentAdded.Designer.cs
+++ UnikProjekt.DatabaseMigration/Migrations/20240524082241_documentAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UnikProjekt.Infrastructure.Database;
 
@@ -11,9 +12,11 @@ using UnikProjekt.Infrastructure.Database;
 namespace UnikProjekt.DatabaseMigration.Migrations
 {
     [DbContext(typeof(UnikDbContext))]
-    partial class UnikDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240524082241_documentAdded")]
+    partial class documentAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UnikProjekt.DatabaseMigration/Migrations/20240524082241_documentAdded.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UnikProjekt.DatabaseMigration.Migrations
+{
+    /// <inheritdoc />
+    public partial class documentAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DocumentContent = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    DocumentTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Documents_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_UserId",
+                table: "Documents",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Documents");
+        }
+    }
+}
