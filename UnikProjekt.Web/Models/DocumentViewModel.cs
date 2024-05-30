bf2b5823f24e7dc905d6d5f8cc6eff34179d9e99
anhtran22 <anhtran_91@hotmail.com>
2024-05-30 15:12:19 +0200

    The old Web was deleted plus updated UI for document

--- UnikProjekt.Web/Models/DocumentViewModel.cs
+++ UnikProjekt.Web/Models/DocumentViewModel.cs
@@ -7,18 +7,17 @@ namespace UnikProjekt.Web.Models
         [Required(ErrorMessage = "Bruger ID er påkrævet")]
         public Guid UserId { get; set; }
 
-        [Display(Name = "Document indhold")]
+        [Display(Name = "Dokument indhold")]
         public IFormFile DocumentContent { get; set; } // IFormFile bruges til filupload i ASP.NET Core
 
         [Required]
         [StringLength(100, ErrorMessage = "Title kan ikke være længere end 100 tegn.")]
-        [Display(Name = "Document Titel")]
+        [Display(Name = "Dokument Titel")]
         public string DocumentTitle { get; set; }
 
-        [Display(Name = "Uploaded af: ")]
+        [Display(Name = "Oploadet af: ")]
         public string UserName { get; set; }
 
-        // Dato for sidste ændring
         [Display(Name = "Ændringsdato")]
         public DateTime DateModified { get; set; }
     }
